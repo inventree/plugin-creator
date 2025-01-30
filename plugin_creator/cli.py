cb2ff89cfc57622d6e07a50d4345693e24762b79
Oliver Walters <oliver.henry.walters@gmail.com>
2025-01-30 04:37:56 +0000

    Add some stuff

---
+++ plugin_creator/cli.py
@@ -0,0 +1,92 @@
+
+import license
+import questionary
+from cookiecutter.main import cookiecutter
+
+import validators
+from helpers import error, info, success, warning
+
+
+def gather_info() -> dict:
+    """Gather project information from the user."""
+
+    context = {}
+
+    # Basic project information
+    context['plugin_title'] = questionary.text(
+        "Enter plugin name",
+        default="Custom InvenTree Plugin",
+        validate=validators.ProjectNameValidator,
+    ).ask().strip()
+
+    context['plugin_description'] = questionary.text(
+        "Enter plugin description",
+        default="A custom InvenTree plugin",
+        validate=validators.NotEmptyValidator,
+    ).ask().strip()
+
+    context['plugin_name'] = context['plugin_title'].replace(" ", "")
+
+    # Convert the project name to a package name
+    # e.g. 'Custom Plugin' -> 'custom_plugin'
+    context['plugin_slug'] = context['plugin_title'].replace(" ", "-").lower()
+    context['package_name'] = context['plugin_slug'].replace("-", "_")
+
+    success(f"Generating plugin '{context['plugin_title']}' - {context['plugin_description']}")
+    info(f" - Package Name: {context['package_name']}")
+
+    info("Enter author information:")
+
+    context['author_name'] = questionary.text(
+        "Author name",
+        default="John Doe",
+        validate=validators.NotEmptyValidator,
+    ).ask().strip()
+
+    context["author_email"] = questionary.text(
+        "Author email",
+        default="email@domain.org",
+    ).ask().strip()
+
+    context["project_url"] = questionary.text(
+        "Project URL",
+        default="",
+    ).ask().strip()
+
+    # Extract license information
+    available_licences = [l for l in license.iter()]
+    license_keys = [l.id for l in available_licences]
+
+    context['license_key'] = questionary.select(
+        "Select a license",
+        default="MIT",
+        choices=license_keys,
+    ).ask()
+
+    context['license_text'] = license.find(
+        context['license_key']
+    ).render(
+        name=context['author_name'],
+        email=context['author_email'],
+    )
+
+    return context
+
+def main():
+    """Run plugin scaffolding."""
+
+    info("InvenTree Plugin Creator Tool")
+
+    context = gather_info()
+
+    # Run cookiecutter template
+    cookiecutter(
+        'plugin-cookiecutter/',
+        no_input=True,
+        output_dir=context['plugin_name'],
+        extra_context=context,
+        overwrite_if_exists=True
+    )
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+from plugin_creator import PLUGIN_CREATOR_VERSION
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="inventree-plugin-creator",
+    version=PLUGIN_CREATOR_VERSION,
+    author="Oliver Walters",
+    author_email="oliver.henry.walters@gmail.com",
+    description="InvenTree plugin creator",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords="inventree plugin scaffold",
+    url="https://github.com/inventree/plugin-creator",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'cookiecutter',
+        'questionary',
+    ],
+    setup_requires=[
+        "wheel",
+        "twine",
+    ],
+    python_requires=">=3.9",
+    entry_points={
+        "console_scripts": [
+            "create-inventree-plugin = plugin_creator.cli:main"
+        ]
+    }
+)
