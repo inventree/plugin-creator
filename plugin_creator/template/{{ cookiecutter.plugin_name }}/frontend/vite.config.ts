62f24331a9b34ea25b87769039df466272839712
Oliver Walters <oliver.henry.walters@gmail.com>
2025-04-02 03:39:23 +0000

    Adjust vite config

--- plugin_creator/template/{{ cookiecutter.plugin_name }}/frontend/vite.config.ts
+++ plugin_creator/template/{{ cookiecutter.plugin_name }}/frontend/vite.config.ts
@@ -1,17 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteExternalsPlugin } from 'vite-plugin-externals'
 
 /**
  * Vite config to build the frontend plugin as an exported module.
  * This will be distributed in the 'static' directory of the plugin.
  */
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: 'classic'
+    }),
+    viteExternalsPlugin({
+      react: 'React',
+      'react-dom': 'ReactDOM',
+      '@mantine/core': 'MantineCore'
+    }),
+  ],
+  esbuild: {
+    jsx: 'preserve',
+  },
   build: {
+    // minify: false,
     cssCodeSplit: false,
-    manifest: false,
+    manifest: true,
+    sourcemap: true,
     rollupOptions: {
       preserveEntrySignatures: "exports-only",
       input: [
@@ -26,7 +39,16 @@ export default defineConfig({
         dir: '../{{ cookiecutter.package_name }}/static',
         entryFileNames: '[name].js',
         assetFileNames: 'assets/[name].[ext]',
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          '@mantine/core': 'MantineCore',
+        },
       },
+      external: ['react', 'react-dom', '@mantine/core'],
     }
-  }
+  },
+  optimizeDeps: {
+    exclude: ['react', 'react-dom', '@mantine/core'],
+  },
 })
