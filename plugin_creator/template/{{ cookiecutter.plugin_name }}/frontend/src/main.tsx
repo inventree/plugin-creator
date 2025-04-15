09b93c119f0361868e237a13287ceac8ab3fb0d0
Oliver Walters <oliver.henry.walters@gmail.com>
2025-04-15 14:25:19 +0000

    Update code

--- plugin_creator/template/{{ cookiecutter.plugin_name }}/frontend/src/main.tsx
+++ plugin_creator/template/{{ cookiecutter.plugin_name }}/frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core'
 
 createRoot(document.getElementById('root')!).render(
