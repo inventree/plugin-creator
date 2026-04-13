9158edbe5bc24fdbb7fc60411e6ae494694b1d97
Oliver Walters <oliver.henry.walters@gmail.com>
2026-04-13 09:32:41 +0000

    Update panel to include custom table support

--- plugin_creator/template/{{ cookiecutter.plugin_name }}/frontend/src/Panel.tsx
+++ plugin_creator/template/{{ cookiecutter.plugin_name }}/frontend/src/Panel.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useMemo, useState } from 'react';
-import { Alert, Button, Group, SimpleGrid, Stack, Text, Title } from '@mantine/core';
+import { Accordion, Alert, Button, Group, SimpleGrid, Stack, Text, Title } from '@mantine/core';
 import { notifications } from '@mantine/notifications';
 {% if "UrlsMixin" in cookiecutter.plugin_mixins.mixin_list -%}
 import { useQuery } from '@tanstack/react-query';
@@ -13,6 +13,9 @@ import { LocalizedComponent } from './locale';
 // Import for type checking
 import { checkPluginVersion, type InvenTreePluginContext } from '@inventreedb/ui';
 import { ApiEndpoints, apiUrl, ModelType } from '@inventreedb/ui';
+
+// Import table display function
+import { InvenTreeTable, useTable, RowEditAction } from '@inventreedb/ui';
 
 /**
  * Render a custom panel with the provided context.
@@ -36,6 +39,42 @@ function {{ cookiecutter.plugin_name }}Panel({
     const partId = useMemo(() => {
         return context.model == ModelType.part ? context.id || null: null;
     }, [context.model, context.id]);
+
+    // Does this InvenTree version support tables in plugins?
+    const supportsTables = useMemo(() => !!context.tables, [context.tables]);
+
+    // State management for the API driven table
+    const tableState = useTable('my-custom-table');
+
+    // Custom table properties for the loaded table
+    const tableProps = {
+        enableSelection: true,
+        enablePagination: true,
+        enableRefresh: true,
+        modelType: ModelType.part,
+        params: {
+            active: true
+        },
+        tableFilters: [
+            {
+                name: 'assembly',
+                label: 'Assembly',
+                description: 'Show assembly parts',
+                type: 'boolean',
+            }
+        ],
+        rowActions: (record: any) => [
+            RowEditAction({
+                onClick: () => {
+                    notifications.show({
+                        title: 'Row Action Clicked',
+                        message: `You clicked the edit action for ${record.name}`,
+                        color: 'blue',
+                    });
+                }
+            })
+        ]
+    };
 
     // Hello world - counter example
     const [ counter, setCounter ] = useState<number>(0);
@@ -99,63 +138,105 @@ function {{ cookiecutter.plugin_name }}Panel({
     return (
         <>
         {editPartForm.modal}
-        <Stack gap="xs">
-        <Title c={context.theme.primaryColor}  order={3}>{{ cookiecutter.plugin_title }}</Title>
-        <Text>
-            This is a custom panel for the {{ cookiecutter.plugin_name }} plugin.
-        </Text>
-        <SimpleGrid cols={2}>
-            {% if cookiecutter.frontend.translation -%}
-            <Alert title='Translated Text' color='grape'>
-                <Stack gap='xs'>
-                    <Text>{t`Translated text, provided by custom code!`}</Text>
-                    <Text>{t`Translations are loaded automatically.`}</Text>
-                    <Text>{t`Fallback locale is used if no translation is available`}</Text>
-                </Stack>
-            </Alert>
-            {%- endif %}
-            <Group justify='apart' wrap='nowrap' gap='sm'>
-                <Button color='blue' onClick={gotoDashboard}>
-                    Go to Dashboard
-                </Button>
-                {partId && <Button color='green' onClick={openForm}>
-                    Edit  Part
-                </Button>}
-                <Button onClick={() => setCounter(counter + 1)}>
-                    Increment Counter
-                </Button>
-                <Text size='xl'>Counter: {counter}</Text>
-            </Group>
-            {instance ? (
-                <Alert title="Instance Data" color="blue">
-                    {instance}
-                </Alert>
-            ) : (
-                <Alert title="No Instance" color="yellow">
-                    No instance data available
-                </Alert>
-            )}
-            {% if "UrlsMixin" in cookiecutter.plugin_mixins.mixin_list -%}
-            {apiQuery.isFetched && apiQuery.data && (
-            <Alert color="green" title="API Query Data">
-                    {apiQuery.isFetching || apiQuery.isLoading ? (
-                    <Text>Loading...</Text>
-                    ) : (
+        <Accordion defaultValue='main'>
+        <Accordion.Item value='main'>
+            <Accordion.Control>
+                <Title c={context.theme.primaryColor}  order={4}>{{ cookiecutter.plugin_title }}</Title>
+            </Accordion.Control>
+        <Accordion.Panel>
+            <SimpleGrid cols={2}>
+                {% if cookiecutter.frontend.translation -%}
+                <Alert title='Translated Text' color='grape'>
                     <Stack gap='xs'>
-                        <Text>Part Count: {apiQuery.data.part_count}</Text>
-                        <Text>Today: {apiQuery.data.today}</Text>
-                        <Text>Random Text: {apiQuery.data.random_text}</Text>
-                        <Button
-                            disabled={apiQuery.isFetching || apiQuery.isLoading}
-                            onClick={() => apiQuery.refetch()}>
-                            Reload Data
-                        </Button>
+                        <Text>{t`Translated text, provided by custom code!`}</Text>
+                        <Text>{t`Translations are loaded automatically.`}</Text>
+                        <Text>{t`Fallback locale is used if no translation is available`}</Text>
                     </Stack>
+                </Alert>
+                {%- endif %}
+                <Group justify='apart' wrap='nowrap' gap='sm'>
+                    <Button color='blue' onClick={gotoDashboard}>
+                        Go to Dashboard
+                    </Button>
+                    {partId && <Button color='green' onClick={openForm}>
+                        Edit  Part
+                    </Button>}
+                    <Button onClick={() => setCounter(counter + 1)}>
+                        Increment Counter
+                    </Button>
+                    <Text size='xl'>Counter: {counter}</Text>
+                </Group>
+                {instance ? (
+                    <Alert title="Instance Data" color="blue">
+                        {instance}
+                    </Alert>
+                ) : (
+                    <Alert title="No Instance" color="yellow">
+                        No instance data available
+                    </Alert>
                 )}
-            </Alert>
-            )}{%- endif %}
-        </SimpleGrid>
-        </Stack>
+                {% if "UrlsMixin" in cookiecutter.plugin_mixins.mixin_list -%}
+                {apiQuery.isFetched && apiQuery.data && (
+                <Alert color="green" title="API Query Data">
+                        {apiQuery.isFetching || apiQuery.isLoading ? (
+                        <Text>Loading...</Text>
+                        ) : (
+                        <Stack gap='xs'>
+                            <Text>Part Count: {apiQuery.data.part_count}</Text>
+                            <Text>Today: {apiQuery.data.today}</Text>
+                            <Text>Random Text: {apiQuery.data.random_text}</Text>
+                            <Button
+                                disabled={apiQuery.isFetching || apiQuery.isLoading}
+                                onClick={() => apiQuery.refetch()}>
+                                Reload Data
+                            </Button>
+                        </Stack>
+                    )}
+                </Alert>
+                )}{%- endif %}
+            </SimpleGrid>
+        </Accordion.Panel>
+        </Accordion.Item>
+        <Accordion.Item value='table'>
+            <Accordion.Control>
+                <Title c={context.theme.primaryColor}  order={4}>Custom Table Example</Title>
+            </Accordion.Control>
+        <Accordion.Panel>
+        {
+            supportsTables ? (
+                <InvenTreeTable
+                    url={apiUrl(ApiEndpoints.part_list)}
+                    tableState={tableState}
+                    context={context}
+                    props={tableProps}
+                    columns={[
+                        {
+                            accessor: 'name',
+                            switchable: false,
+                        },
+                        {
+                            accessor: 'IPN',
+                        },
+                        {
+                            accessor: 'description',
+                        }
+                    ]}
+                />
+            ) : (
+                <Alert title="Table Not Supported" color="red">
+                    {
+                        'This version of InvenTree does not support tables within plugins.'
+                    }
+                    <br />
+                    {
+                        'Please upgrade to a more recent version of InvenTree to use this feature.'
+                    }
+                </Alert>
+            )
+        }
+        </Accordion.Panel>
+        </Accordion.Item>
+        </Accordion>
         </>
     );
 }
